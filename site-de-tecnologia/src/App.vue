a1303081778f02e520df83b7c69dbca14ee4642f
Gabriel Gomes <gabrielglacerda@hotmail.com>
2022-05-11 16:53:21 -0300

    feature/base do projeto

---
+++ site-de-tecnologia/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header></Header>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Header from './components/Header.vue'
+
+export default defineComponent({
+    components: { Header }
+})
+</script>
+
+<style >
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+</style>
---
+++ site-de-tecnologia/src/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <nav id="nav-bar">
+            <div class="content">
+                <img id="logo" src="../assets/logo2.png" alt="logo">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/services">Serviços</a></li>
+                    <li><a href="/about-us">Sobre nós</a></li>
+                    <li><a href="/contact">Contato</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    
+})
+</script>
+
+<style scoped>
+    #nav-bar {
+        background-color: #F2F2F2;
+        height: 160px;
+        overflow: hidden;
+    }
+
+    .content {
+        display: grid;
+        grid-template-columns: 4fr 3fr;
+        align-items: center;
+    }
+
+    ul {
+        list-style: none;
+        display: flex;
+        padding-bottom: 50px;
+    }
+
+    a {
+        text-decoration: none;
+        font-weight: bold;
+        font-size: 1.6rem;
+        padding: 1rem;
+    }
+
+    #logo {
+        height: 200px;
+        width: 200px;
+    }
+</style>
